5ff5585bf1db8165ecfd45bab7e6b9c6bd85946e
marc <marcsagabcn@gmail.com>
2023-12-18 21:50:55 +0100

    add vercel speed insights package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
 import Head from "next/head";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "~/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -19,6 +18,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
+      <SpeedInsights />
     </SessionProvider>
   );
 };
