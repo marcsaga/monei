eefb199e1d900973935aa7c00e5e870c5b25c01b
marc <marcsagabcn@gmail.com>
2023-12-27 20:18:56 +0100

    remove total value card

--- src/pages/monthly-view/investments.tsx
+++ src/pages/monthly-view/investments.tsx
@@ -12,22 +12,16 @@ import { type Investment } from "~/utils/interfaces";
 export default function IncomesMonthly() {
   const { filters } = useMonthlyFilters();
   const invested = useMonthlyInvestedTotals();
-  const valueTotals = useMonthlyValueTotals();
 
   return (
     <MonthlyLayout>
       <TotalCard
         title={getMonthName(filters.start)}
-        description="Total invested"
+        description="Total contributions"
         total={invested.currentTotal}
         previousTotal={invested.previousTotal}
       />
-      <TotalCard
-        title={getMonthName(filters.start)}
-        description="Total value"
-        total={valueTotals.currentTotal}
-        previousTotal={valueTotals.previousTotal}
-      />
+      <span />
       <div className="col-span-2">
         <InvestmentTable />
       </div>
@@ -44,21 +38,6 @@ function useMonthlyInvestedTotals() {
 
   const calculate = (acc: number, expense: Investment) =>
     acc + (expense.contribution ?? 0);
-  return {
-    currentTotal: currentMonthExpense.data?.reduce(calculate, 0) ?? 0,
-    previousTotal: previousMonthExpense.data?.reduce(calculate, 0) ?? 0,
-  };
-}
-
-function useMonthlyValueTotals() {
-  const current = useMonthlyFilters().filters;
-  const previous = getFullPreviousMonthDates(current.start);
-
-  const currentMonthExpense = useListInvestments(current);
-  const previousMonthExpense = useListInvestments(previous);
-
-  const calculate = (acc: number, expense: Investment) =>
-    acc + (expense.totalValue ?? 0);
   return {
     currentTotal: currentMonthExpense.data?.reduce(calculate, 0) ?? 0,
     previousTotal: previousMonthExpense.data?.reduce(calculate, 0) ?? 0,
