acb097c294202a544c648a01ce7614b44f622ff5
marc <marcsagabcn@gmail.com>
2023-12-30 00:07:33 +0100

    setup sha components

---
+++ src/components/user-menu.tsx
@@ -0,0 +1,12 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/sha-components/ui/dropdown-menu";
+
+const UserMenu = () => {
+  return <DropdownMenu></DropdownMenu>;
+};
